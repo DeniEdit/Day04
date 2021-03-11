b260821bfaab8cf51dfd6704c06542c5f684192c
DeniEdit <milicevic.deni@gmail.com>
2021-03-11 14:08:00 +0100

    Decimal Numbers

---
+++ ex04/src/index.js
@@ -0,0 +1,15 @@
+function main(){
+    //only change code below this line
+    var myDecimal=2.5,
+        myDecimalE=0.025;
+
+    //Only change code above this line
+
+    return{
+        myDecimal,
+        myDecimalE
+    };
+}
+
+console.log(main());
+module.exports = main;
