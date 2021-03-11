8bce5b49d8ba82a7085ef562f705c4e794694e94
DeniEdit <milicevic.deni@gmail.com>
2021-03-11 16:31:32 +0100

    Exercise 10

---
+++ ex10/src/index.js
@@ -0,0 +1,18 @@
+function main(){
+    //only change code below this line
+
+    myFirstName="Deni",
+    myLastName="Milicevic",
+    myHomeTown="Mostar";
+
+    //Only change code above this line
+
+    return {
+        myFirstName, 
+        myLastName, 
+        myHomeTown 
+    };
+}
+
+console.log(main()); 
+module.exports=main;
