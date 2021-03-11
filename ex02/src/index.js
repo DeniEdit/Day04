79d79aa3ca469f49f5a207add881e27f75196a3e
DeniEdit <milicevic.deni@gmail.com>
2021-03-11 14:01:02 +0100

    Increment(++)

--- ex02/src/index.js
+++ ex02/src/index.js
@@ -3,7 +3,7 @@ function main(){
     var myRes=75;
     
         //Only change code below this line
-        myRes = myRes+1;
+        myRes ++;
 
         //Only change code below this line
         return myRes;
